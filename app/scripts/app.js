7c74a4f9209fa5ab087cb3edb874c3b51db68362
Kostas Siabanis <kostas.siabanis@gmail.com>
2016-06-08 09:17:00 +0300

    Complete game logic

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,6 +5,18 @@ var React = window.React = require('react'),
     mountNode = document.getElementById("app"),
     winningCombos = ['001010100', '100010001', '010010010', '111000000', '000111000', '000000111', '001001001', '100100100'];
 
+var Footer = React.createClass({
+    render: function () {
+        return (
+            //TODO: class status-bar
+            <div className="status-bar">
+                <div>{this.props.statusText}
+                    <button className={this.props.moveNum > 0 ? "visible-xs pull-right" : "hidden-xs"} onClick={this.props.startOver}>Start Over</button>
+                </div>
+            </div>
+        );
+    }
+});
 
 var TicTacToe = React.createClass({
 
@@ -22,30 +34,44 @@ var TicTacToe = React.createClass({
             tiles: ['', '', '', '', '', '', '', '', ''],
             turn: 0,
             moveNum: 0,
-            winnerFound: false
+            winnerFound: false,
+            statusText: ''
         };
     },
 
     render: function () {
         return (
-            <div className="ticTacToe">
-                { this.state.tiles.map(function (tile, position) {
-                    return (
-                        <Tile status={tile} key={position} position={position} turn={this.state.turn}
-                              tileClick={this.tileClick}/>
-                    );
-                }, this) }
+            <div className="container">
+                <div className="ticTacToe blur">
+                    { this.state.tiles.map(function (tile, position) {
+                        return (
+                            <Tile status={tile} key={position} position={position} turn={this.state.turn}
+                                  tileClick={this.tileClick}/>
+                        );
+                    }, this) }
+                </div>
+                <Footer statusText={ !this.state.statusText ? 'Player ' + (this.state.turn === 0 ? 'X' : 'O') + ' playing' : this.state.statusText}
+                        startOver={this.startOver}
+                        moveNum={this.state.moveNum}
+                />
             </div>
         );
     },
 
     checkWin: function (player) {
         var nMove = parseInt(this.state.players[player].moves.join('').split('').reverse().join(''), 2);
-        winningCombos.map(function(combo){
+        winningCombos.map(function (combo) {
             var nCombo = parseInt(combo, 2);
             if ((nMove & nCombo) === nCombo) {
-                this.setState({winnerFound: player === 0 ? 'X' : 'O'});
-                console.log("Player " + (player === 0 ? 'X' : 'O') + ' wins!');
+                this.setState({
+                    winnerFound: player === 0 ? 'X' : 'O',
+                    statusText: "Player " + (player === 0 ? 'X' : 'O') + " wins!"
+                });
+            }
+            if (this.state.moveNum === 8 && !this.state.winnerFound) {
+                this.setState({
+                    statusText: "Nobody wins"
+                });
             }
         }, this);
     },
@@ -53,6 +79,7 @@ var TicTacToe = React.createClass({
     tileClick: function (position, player) {
         var tiles = this.state.tiles;
         var players = this.state.players;
+        var moveNum = this.state.moveNum;
         if (!tiles[position] && !this.state.winnerFound) {
             tiles[position] = player === 0 ? 'X' : 'O';
             players[player].moves[position] = 1;
@@ -60,12 +87,13 @@ var TicTacToe = React.createClass({
                 players: players,
                 tiles: tiles,
                 turn: player === 0 ? 1 : 0,
-                checkWin: this.checkWin(player)
+                checkWin: this.checkWin(player),
+                moveNum: moveNum + 1
             });
         }
     },
 
-    startOver: function() {
+    startOver: function () {
         this.setState(this.getInitialState());
     }
 
